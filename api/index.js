f258fb517d3aeb8eec77f70a99a0b63f72580718
jd2rogers2 <jd2rogers2@gmail.com>
2023-09-08 10:09:43 -0400

    sending and receiving data successfully

--- api/index.js
+++ api/index.js
@@ -3,14 +3,24 @@ const cors = require('cors');
 
 
 let corsOptions = {
-   origin : ['http://localhost:3001'],
+  origin : ['http://localhost:3001'],
 };
 
 const app = express();
 app.use(cors(corsOptions));
+app.use(express.json());
+
+const data = [];
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  console.log('data', data);
+  res.send({ data });
+});
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body);
+  data.push(req.body)
+  res.send(true);
 });
 
 const port = 3000;
--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 // import logo from './logo.svg';
 import './App.css';
@@ -6,21 +6,54 @@ import './App.css';
 const REACT_APP_HH_API_URL = process.env.REACT_APP_HH_API_URL;
 
 function App() {
-  const [test, setTest] = useState('');
+  const [data, setData] = useState();
+  const [formData, setFormData] = useState({});
 
-  useEffect(() => {
-    const testData = async () => {
-      const res = await fetch(`http://${REACT_APP_HH_API_URL}/`);
-      const data = await res.text();
-      setTest(data);
-    }
-    testData();
-  }, []);
+  const getData = async () => {
+    const res = await fetch(`http://${REACT_APP_HH_API_URL}/`);
+    const data = await res.text();
+    setData(data);
+  }
+
+  const handleTextChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await fetch(`http://${REACT_APP_HH_API_URL}/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        host_id: "host_id",
+        attendee_limit: 5,
+        location: "location",
+        start_time: "2023-09-08",
+        end_time: "2023-10-31",
+        image_url: "image_url",
+        description: "description",
+        ...formData,
+      }),
+    });
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{test}</p>
+        <h1>get data</h1>
+        <p>data: {data}</p>
+        <button onClick={getData}>refresh data</button>
+        <h1>send data</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" value={formData.name || ''} onChange={handleTextChange} />
+          <button>submit</button>
+        </form>
       </header>
     </div>
   );
